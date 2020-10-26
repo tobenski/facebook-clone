f5df9844f1e4ad495f0f6dab04f6e7655bc09d7b
Knud Rishøj <knud@det-gamle-posthus.dk>
2020-10-26 09:17:15 +0100

    added Taliwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ ${APP_NAME} }} | @section('title', '')</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
 </head>
 <body>
 
